00401e998e42c4fff9ac07769612805f60fb5f7c
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-05-01 17:17:29 +0000

    refactor(`ConfigurableCollectionAdapter`): update element type inference for improved clarity and consistency

--- src/lib/configurable-collection.adapter.ts
+++ src/lib/configurable-collection.adapter.ts
@@ -2,8 +2,8 @@
 import { CollectionSettings } from "@typedly/collection";
 import { ConfigurableCollectionShape } from "./configurable-collection.shape";
 // Type.
-import { InferAsync } from "@typedly/data";
-import { InferCollectionType, InferElement } from "@typedly/collection";
+import { InferAsync, IterableElement } from "@typedly/data";
+import { InferCollectionType } from "@typedly/collection";
 /**
  * @description The `ConfigurableCollectionAdapter` interface defines a adapter collection data structure that can be configured based on the provided settings.
  * It extends the `ConfigurableCollectionShape` interface, allowing it to inherit collection-specific functionalities while also supporting configurability through the settings pattern.
@@ -11,14 +11,14 @@ import { InferCollectionType, InferElement } from "@typedly/collection";
  * @interface ConfigurableCollectionAdapter
  * @template {CollectionSettings<T, E, R>} C The collection settings type.
  * @template {Iterable<E>} [T=InferCollectionType<C>] The type of the collection inferred from the collection settings or defaults to `unknown` if not specified.
- * @template [E=InferElement<C>] The element type inferred from the collection settings or defaults to `unknown` if not specified.
+ * @template [E=IterableElement<T>] The element type inferred from the collection type `T`.
  * @template {boolean} [R=InferAsync<C>] The async behavior flag inferred from the collection settings or defaults to `false` if not specified.
  * @extends {ConfigurableCollectionShape<C, T, E, R>}
  */
 export interface ConfigurableCollectionAdapter<
   C extends CollectionSettings<T, E, R>,
   T extends Iterable<E> = InferCollectionType<C>,
-  E = InferElement<C>,
+  E = IterableElement<T>,
   R extends boolean = InferAsync<C>,
 > extends ConfigurableCollectionShape<C, T, E, R> {
   readonly version: string;
